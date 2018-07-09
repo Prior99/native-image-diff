fbf35453a42b90718f49264ca237c7fe56494270
Attila Bartha <atikenny@gmail.com>
2018-07-09 10:47:22 +0100

    rename binary site to base url, rename env var to NATIVE_IMAGE_DIFF_BINARY_URL

--- scripts/install.js
+++ scripts/install.js
@@ -9,8 +9,8 @@ if (process.env["NATIVE_IMAGE_DIFF_SKIP_BINARY_DOWNLOAD_FOR_CI"]) { return; }
 // Get the version of the library;
 const pkg = require(path.resolve(__dirname, "..", "package.json"));
 const packageVersion = pkg.version;
-const binarySite = process.env.NID_BINARY_SITE || "https://github.com/Prior99/native-image-diff/releases/download";
-const url = `${binarySite}/${packageVersion}/${fileName.baseName}`;
+const baseUrl = process.env.NATIVE_IMAGE_DIFF_BINARY_URL || "https://github.com/Prior99/native-image-diff/releases/download";
+const url = `${baseUrl}/${packageVersion}/${fileName.baseName}`;
 
 console.info(`Downloading native-image-diff prebuilt binary from "${url}".`);
 
